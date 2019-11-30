33bace7d865407ad332b63dc74c5c6e42349721f
Patrick McMahon <pmcmahon14@gmail.com>
2019-11-29 22:42:27 -0800

    finished Operators

--- Operators/src/com/company/Main.java
+++ Operators/src/com/company/Main.java
@@ -3,6 +3,35 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        //play with other operators as well
+        System.out.println("previousResult =  " + previousResult);
+        result *= 10;
+        System.out.println("2 * 10 = " + result);
+        result /= 5;
+        System.out.println("20 / 5 = " + result);
+        result %= 3;
+        System.out.println("4 % 3 = " + result);
+
+        result++;
+        System.out.println("1 + 1 = " + result);
+
+        result--;
+        System.out.println("2 - 1 = " + result);
+
+        result += 2;
+        System.out.println("1 + 2 = " + result);
+        result *= 10;
+        System.out.println("3 * 10 = " + result);
+        result /= 3;
+        System.out.println("30 / 3 = " + result);
+        result -= 2;
+        System.out.println("10 - 2 = " + result);
     }
 }
