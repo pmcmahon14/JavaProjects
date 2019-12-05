351cfbc32b1f1e6bf1329288d5d29201c2076999
Patrick McMahon <pmcmahon14@gmail.com>
2019-12-04 22:07:13 -0800

    finished exercise

--- PositiveNegativeZero/src/com/company/Main.java
+++ PositiveNegativeZero/src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        checkNumber(0);
+    }
+
+    public static void checkNumber(int number) {
+        if (number > 0) {
+            System.out.println("positive");
+        } else if (number < 0) {
+            System.out.println("negative");
+        } else {
+            System.out.println("zero");
+        }
     }
 }
