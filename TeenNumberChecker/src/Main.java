dfa17ad70aa5cbaa8ad71118a8d31fee009e1f86
Patrick McMahon <pmcmahon14@gmail.com>
2019-12-08 17:09:10 -0800

    finished project

--- TeenNumberChecker/src/Main.java
+++ TeenNumberChecker/src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        TeenNumberChecker.hasTeen(9, 99, 19);
+        TeenNumberChecker.isTeen(13);
     }
 }
--- TeenNumberChecker/src/TeenNumberChecker.java
+++ TeenNumberChecker/src/TeenNumberChecker.java
@@ -1,2 +1,24 @@
 public class TeenNumberChecker {
+
+    public static boolean hasTeen(int n1, int n2, int n3) {
+
+        if ((n1 > 12 && n1 < 20) || (n2 > 12 && n2 < 20) || (n3 > 12 && n3 < 20)) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+    }
+
+    public static boolean isTeen(int n1){
+
+        if (n1 > 12 && n1 < 20) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+    }
 }
