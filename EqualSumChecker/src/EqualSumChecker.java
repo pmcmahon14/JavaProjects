c3e73ad3655617491dd6ff8599bd324b0c4491a7
Patrick McMahon <pmcmahon14@gmail.com>
2019-12-08 16:51:51 -0800

    finished project

--- EqualSumChecker/src/EqualSumChecker.java
+++ EqualSumChecker/src/EqualSumChecker.java
@@ -1,2 +1,15 @@
 public class EqualSumChecker {
+
+    public static boolean hasEqualSum(int n1, int n2, int n3) {
+
+        int sum = n1 + n2;
+
+        if (sum == n3) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+    }
 }
--- EqualSumChecker/src/Main.java
+++ EqualSumChecker/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        EqualSumChecker.hasEqualSum(1, -1, 0);
     }
 }
