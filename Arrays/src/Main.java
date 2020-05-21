ac385e41145a5416faffd65f78e4c951852773f6
Patrick McMahon <pmcmahon14@gmail.com>
2020-05-20 22:29:34 -0700

    some basic array stuff done

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] myIntArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        System.out.println(myIntArray[0]);
+        System.out.println(myIntArray[6]);
+        System.out.println(myIntArray[8]);
+
+        int[] myIntArray2 = new int[10];
+        for(int i=0; i<myIntArray2.length; i++) {
+            myIntArray[i] = i*10;
+        }
+        for(int i=0; i<myIntArray2.length; i++) {
+            System.out.println("Element " + i + ", value is " + myIntArray[i]);
+        }
+
+        String[]names;
+        names = new String[4];
+        names[0] = "Patrick";
+        names[1] = "Dawn";
+        names[2] = "Finn";
+        names[3] = "Sierra";
+
+        int random = (int) (Math.random()*4);
+
+        System.out.println("The random name is: " + names[random]);
     }
 }
