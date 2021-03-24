3c2c738f058403ec718f8b174480dd62d39556c1
Patrick McMahon <pmcmahon14@gmail.com>
2021-03-23 18:23:38 -0700

    new project

---
+++ Kotlin Notes/Variables.kts
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    println("Hello World")
+    println("My first Kotlin program")
+
+    var patrick: String
+    patrick = "Patrick McMahon"
+    println(patrick)
+
+    var patrickSalary: Int = 32
+    var monthly: Int = patrickSalary * 4
+    println(patrickSalary)
+    println(monthly)
+}
